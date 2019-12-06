e4dee9ff3b382582187e6a03fac8ebe52fe85a43
David Erler <daviderler1995@gmail.com>
2019-12-06 17:47:40 +0100

    add minitest whitelist feature if running with rails 6.0
    The minitest whitelist feature can be used to allow test to fail that
    are on the test/whitelist.yml. The whitelist is only active when
    minitests is running with rails version 6.0 or higher.
    
    The minitest testreportes work as expected and report the tests as
    failing but the minitest exit code is manipulated and 0 if all tests are
    passing or the failing tests are on the whitelist.
    
    The exit code is 1 if there are additional failures.
    
    The junit reporter creates the normal xml output and the ci pipeline
    might fail if it uses the test reports to determine if the testsuit run
    was successfull. Please use only the exit code.

--- test/lib/simple_test.rb
+++ test/lib/simple_test.rb
@@ -3,7 +3,11 @@
 require 'test_helper'
 
 class SimpleTest < ActiveSupport::TestCase
-  def test_simple
+  def test_assertion
     assert Rails.gem_version < Gem::Version.new('6')
+  end
+
+  def test_raise
+    assert true
   end
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -12,4 +12,31 @@ class ActiveSupport::TestCase
 end
 
 require "minitest/reporters"
+class Minitest::Reporters::BaseReporter
+  def passed?
+    return self.tests.find { |test| test.failure } if Rails.gem_version < Gem::Version.new('6')
+    failed_test_identifier_strings = []
+
+    failed_tests = self.tests.select(&:failure)
+    failed_tests.each do |t|
+      test_identifier_string = "#{t.klass}##{t.name}"
+      failed_test_identifier_strings << test_identifier_string
+    end
+    test_whitelist = YAML::load_file(File.join(__dir__, 'whitelist.yml'))
+    failed_tests = failed_test_identifier_strings.reject { |t| test_whitelist.include?(t) }
+    unless failed_tests.empty?
+      puts "The following failed tests are not part of the whitelist:"
+      failed_tests.each { |test| puts "- #{test}"}
+      return false
+    end
+    passed_tests = self.tests.select(&:passed?)
+    passed_whitelisted_tests = test_whitelist.select { |t| passed_tests.include?(t) }
+    unless passed_whitelisted_tests.empty?
+      puts "The following passing tests are on the whitelist. Please cleanup the whitelist"
+      passed_whitelisted_tests.each { |test| puts "- #{test}"}
+    end
+
+    true    
+  end
+end
 Minitest::Reporters.use! [Minitest::Reporters::DefaultReporter.new, Minitest::Reporters::JUnitReporter.new]
