335ad7b47a29dda138dd262b831ad11f3be480d2
David Erler <daviderler1995@gmail.com>
2019-12-07 12:05:50 +0100

    run rubocop -a and --auto-generate-config

--- lib/rack/health_check.rb
+++ lib/rack/health_check.rb
@@ -1,4 +1,3 @@
-
 # frozen_string_literal: true
 
 # Your Ruby Rack App that returns your Rails App's Health
@@ -6,20 +5,20 @@
 # "lib/rack/health_check.rb"
 
 module Rack
-  class  HealthCheck
-    def call(env)
+  class HealthCheck
+    def call(_env)
       status = {
         # postgres: {
         #   connected: postgres_connected,
         #   migrations_updated: postgres_migrations_updated,
         #   rails_version: Rails.version
-        # }, 
+        # },
         version: {
           rails: Rails.version
         }
       }
 
-      return [200, {}, [status.to_json]]
+      [200, {}, [status.to_json]]
     end
 
     protected
@@ -28,7 +27,7 @@ module Rack
       ApplicationRecord.establish_connection
       ApplicationRecord.connection
       ApplicationRecord.connected?
-    rescue => e
+    rescue StandardError => e
       false
     end
 
